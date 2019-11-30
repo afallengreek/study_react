2f9dbf692ca5e19c67cdcf120bb92bfaab4b9834
王企鹅啊 <704149236@qq.com>
2019-11-30 19:23:36 +0800

    Save

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,7 @@
-
-
+const { override, addDecoratorsLegacy } = require('customize-cra');
+module.exports = override(
+    addDecoratorsLegacy()
+);
 module.exports = function override(config, env) {
   return config;
 };
